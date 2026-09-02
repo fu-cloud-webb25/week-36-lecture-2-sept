2aadd0cb9f8e930b9990a73678869b735301593c
Santosnr6 <jesper-nyberg@hotmail.com>
2026-09-02 11:23:24 +0200

    feat: add deleteMovie endpoint

--- movievault-api/functions/movies/deleteMovie/index.mjs
+++ movievault-api/functions/movies/deleteMovie/index.mjs
@@ -1,5 +1,27 @@
+import middy from '@middy/core';
+import { authenticateUser } from '../../../middlewares/authenticate.mjs';
+import { authorizeRole } from '../../../middlewares/authorize.mjs';
+import { errorHandler } from '../../../middlewares/errorHandler.mjs';
 import { sendResponse } from "../../../responses/index.mjs";
+import { movies } from '../../../data/movies.mjs';
 
-export const handler = async (event) => {
-  return sendResponse(200, { message : 'Its alive!!!' });
-};
+export const handler = middy(async (event) => {
+  const { id } = event.pathParameters;
+  const index = movies.findIndex(m => m.id === id);
+  if(index !== -1) {
+    const deleted = movies.splice(index, 1);
+    return sendResponse(200, {
+      success : true,
+      message : `Movie with id ${id} deleted successfully`,
+      deleted,
+      movies
+    });
+  } else {
+    return sendResponse(404, {
+      success : false,
+      message : `Movie with id ${id} not found`
+    });
+  }
+}).use(authenticateUser())
+  .use(authorizeRole('admin'))
+  .use(errorHandler());
