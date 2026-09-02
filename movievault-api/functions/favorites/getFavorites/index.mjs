1be1cac60eb6bc636eb2bd8d971d99acb078368c
Santosnr6 <jesper-nyberg@hotmail.com>
2026-09-02 11:33:44 +0200

    feat: add getFavorites endpoint

--- movievault-api/functions/favorites/getFavorites/index.mjs
+++ movievault-api/functions/favorites/getFavorites/index.mjs
@@ -1,5 +1,21 @@
+import middy from '@middy/core';
+import { authenticateUser } from '../../../middlewares/authenticate.mjs';
+import { authorizeRole } from '../../../middlewares/authorize.mjs';
+import { errorHandler } from '../../../middlewares/errorHandler.mjs'; 
 import { sendResponse } from "../../../responses/index.mjs";
+import { favorites } from '../../../data/favorites.mjs';
+import { movies } from '../../../data/movies.mjs';
 
-export const handler = async (event) => {
-  return sendResponse(200, { message : 'Its alive!!!' });
-};
+export const handler = middy(async (event) => {
+  const userFavorites = favorites.filter(f => f.username === event.user.username);
+  const favoriteMovies = userFavorites.map(f => {
+    return movies.find(m => m.id === f.movieId)
+  });
+
+  return sendResponse(200, {
+    success : true,
+    favorites : favoriteMovies
+  });
+}).use(authenticateUser())
+  .use(authorizeRole('user'))
+  .use(errorHandler());
